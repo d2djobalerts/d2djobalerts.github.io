a6888328ff680a8c3024e369bbdca11112ebf327
d2djobalerts <d2djobalerts@gmail.com>
2024-07-21 21:42:48 +0530

    changes

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
-objname = {
-    file1 : "http://127.0.0.1:5500/about-us.html",
-    file2 : "http://127.0.0.1:5500/example.html",
-    file3 : 'http://127.0.0.1:5500/index.html',
-}
+const websiteEnterLink = document.querySelector('.webpage-links-d2djobalerts');
+
+setInterval(function(){
+    websiteEnterLink.style.transform = 'scale(1.03)';
+},1000);
