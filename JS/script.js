50fd8c6eca8537055a6f3ddbff0c8ed5dd0b1e90
d2djobalerts <d2djobalerts@gmail.com>
2024-07-21 22:41:33 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ const websiteEnterLink = document.querySelector('.webpage-links-d2djobalerts');
 
 
 setInterval(function(){
-    websiteEnterLink.style.transform = 'scale(1.03)';
+    websiteEnterLink.style.transform = 'scale(1.01)';
     // websiteEnterLink.style.boxShadow = '-2px 2px 5px 10px blue';
     websiteEnterLink.style.boxShadow = '-1px 1px 2.5px 5px white';
     console.log('down');
