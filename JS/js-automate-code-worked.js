5d89c6ea652fb3df4389e5946012bb997c8a3d8b
d2djobalerts <d2djobalerts@gmail.com>
2024-07-29 22:23:54 +0530

    change

---
+++ JS/js-automate-code-worked.js
@@ -0,0 +1,481 @@
+const socialMediaLinks = {
+    telegram : '',
+    whatsapp : '',
+    insta : 'insagram',
+}
+
+
+const obj = {
+
+    Deloitte_Analyst : {
+        jobTitle: 'APPLY NOW | Deloitte is hiring for Analyst',
+        imgSrc: 'https://d2djobalerts.github.io/images/deloitte.png',
+        imgSrcTelegram: '',
+        imgSrcWhatsapp: '',
+        description: 'Deloitte is looking for Analyst with any Graduate as qualification. The complete Accenture like qualification, skills, etc are provided below.',
+        companyName: 'Deloitte ',
+        role: 'Analyst',
+        location: 'Hyderabad, Bengaluru, Delhi',
+        experience: '0-3 Years',
+        qualification: 'Bachelors/Masters Degree',
+        responsibilities: {
+          1: 'Assess and advise on the data risk elements of global services and technologies based on a defined process addressing privacy, confidentiality, and technical controls.',
+          2: 'Mediating between the technology teams and regional member firm stakeholders to provide clarity.',
+          3: 'Delivering data risk assessments used by regional stakeholders to determine their risk/adoption strategy.',
+          4: 'Managing a portfolio of engagements with competing demands and project deadlines.',
+          5: 'Act as a trusted advisor with business and technology teams, as well as member firm stakeholders, to provide data risk consult and guidance',
+          6: 'Manage ongoing privacy compliance and consultative activities with an eye towards creating process efficiencies wherever possible',
+          7: 'Help define, update, maintain, and enhance privacy compliance and consultative program requirements',
+          8: 'Support privacy consultations and compliance reviews, and oversee remediation activities across the global network',
+          
+        },
+        skillsEligibility: {
+          1: 'Experience in conducting and delivering privacy impact assessments (PIA)',
+          2: 'Privacy, confidentiality, legal, data risk/security and/or technology experience.',
+          3: 'Experience of advising on privacy and confidentiality matters such as working with technology implementation teams',
+          4: 'Strong knowledge and awareness of global privacy laws, rules, and regulations within at least one jurisdiction such as the GDPR, LGPD, CCPA, CPRA, CDPA, CPA, HIPAA, GLBA, and COPPA',
+          5: 'High-level understanding of technology and security issues impacting privacy programs',
+          6: 'Extensive experience and strong understanding of privacy impact assessments (PIA), Privacy/Data by Design concepts, tools, and methodologies',
+          
+        },
+        howToApply: {
+          1: 'Read Job Description',
+          2: 'Click on below APPLY LINK',
+          3: 'Redirects to official page',
+          4: 'Fill formand APPLY'
+        },
+        jobApplyURL: {
+          1: 'https://usijobs.deloitte.com/careersUSI/JobDetail?jobId=190417',
+          2: 'https://jobsindia.deloitte.com/job/Bengaluru-T&T-ET&P-FT-SAP-EPM-Analyst/26133044/',
+          3: 'https://jobsindia.deloitte.com/job/Delhi-T&T-EAD-Fresher-Analyst/26996644/',
+          
+        }
+    },
+
+    Accenture : {
+        jobTitle : 'Apply Now | Accenture is hiring for Data Science Analytics Associate  !',
+        imgSrc : 'https://d2djobalerts.github.io/images/accenture-logo.png',
+        imgSrcTelegram : 'https://d2djobalerts.github.io/images/Telegram-join-image.png',
+        imgSrcWhatsapp : 'https://d2djobalerts.github.io/images/Whatsapp-join-image.png',
+        description : 'Accenture is looking for Data Science Analytics Associate with any Graduate as qualification. The complete Accenture like qualification, skills, etc are provided below.',
+        companyName : 'Accenture',
+        role : 'Data Science Analytics Associate',
+        location : 'Bengaluru, India',
+        experience : 'Freshers/Experienced',
+        qualification : 'any Graduate',
+        
+        responsibilities : {
+            1 : 'Compile and analyze data on New Business, Underwriting (UW), and Licensing & Compliance ',
+            2  : 'Create daily dashboards to monitor application submissions and processing times',
+            3  : 'Develop SQL queries to extract data from cloud services, Utilize Power BI and Excel macros to create data dashboards and support reporting processes',
+            4  : 'Analyze and improve VBA scripts in MS Access',
+            5 : 'Strong problem-solving skills for data cleansing and bug fixing and Effective communication skills for understanding client requirements',
+            
+        },
+    
+        skillsEligibility : {
+            1 : "Understand client requirements and create SQL queries to extract data from cloud services.",
+            2 : 'Create data dashboards using Power BI and Develop and support reporting processes using advanced Excel.',
+            3 : 'Analyze, identify, and fix bugs in VBA (MS Access)',
+            4 : 'Knowledge of R programming is beneficial.',
+            5 : 'Proficient in VBA (Visual Basic for Applications), SQL, advanced Excel, MS Access, and Power BI.',
+            6 : 'Familiarity with R language is a plus',
+        },
+    
+        howToApply : {
+            1 : 'Read Job Description',
+            2 : 'Click on below APPLY LINK',
+            3 : 'Redirects to official page',
+            4 : 'Fill formand APPLY'
+        },
+    
+        jobApplyURL : 'https://www.accenture.com/in-en/careers/jobdetails?src=LINKEDINJP&id=AIOC-S01523713_en#accordion-457aff5aa2-item-959cb67f76',
+        telegramChannelURL : socialMediaLinks.telegram,
+        whatsAppChannelURL : socialMediaLinks.whatsapp,
+        imstagramChannelURL : `${socialMediaLinks.insta}`,
+    },
+    
+    Google : {
+        jobTitle : 'Apply Now | Google is hiring for Data Analyst  !',
+        imgSrc : 'https://d2djobalerts.github.io/images/google-logo.jpg',
+        imgSrcTelegram : 'https://d2djobalerts.github.io/images/Telegram-join-image.png',
+        imgSrcWhatsapp : 'https://d2djobalerts.github.io/images/Whatsapp-join-image.png',
+        description : 'Google is looking for Data Analyst with bachelors/Masters Degree as qualification. The complete Googlemation like qualification, skills, etc are provided below.',
+        companyName : 'Google',
+        role : 'Data Analyst',
+        location : 'Hyderbad, India',
+        experience : 'Experienced',
+        qualification : 'Bachelors degree/Masters Degree',
+        
+        responsibilities : {
+            1 : 'Design, develop, and maintain Trust and Safety access and security systems to meet security goals while optimizing operational efficiency.',
+            2  : 'Implement integrations aligning business objectives with security systems, executing strategies to enhance system effectiveness.',
+            3  : 'Lead projects involving diverse stakeholders, tight deadlines, and critical organizational impact, adapting to evolving conditions.',
+            4  : 'Possess domain expertise in data sources, access methods, authentication, and relevant products within the process area.',
+
+        },
+    
+        skillsEligibility : {
+            1 : "Bachelor's degree or equivalent experience.",
+            2 : '2 years in data analytics, Trust & Safety, policy, cybersecurity, or related fields.',
+            3 : 'Skills in SQL, dashboard building, ETL (Extract, Transform, Load), statistical modeling, and scripting (e.g., Python).',
+            4 : 'Experience using data to derive insights and recommendations.',
+            5 : 'Preferred qualifications:',
+            6 : 'Preferred qualifications are detailedon google career page click on below apply link',
+        },
+    
+        howToApply : {
+            1 : 'Read Job Description',
+            2 : 'Click on below APPLY LINK',
+            3 : 'Redirects to official page',
+            4 : 'Fill formand APPLY'
+        },
+    
+        jobApplyURL : 'https://www.google.com/about/careers/applications/jobs/results/135169036483732166-data-analyst',
+        telegramChannelURL : socialMediaLinks.telegram,
+        whatsAppChannelURL : socialMediaLinks.whatsapp,
+        imstagramChannelURL : `${socialMediaLinks.insta}`,
+    },
+}
+
+
+const value = document.querySelector('#companyNameHtml').textContent;
+console.log(value);
+document.querySelector('#companyNameHtml').style.display = 'none';
+
+for( const a of Object.keys(obj)){
+    console.log('aaaaa'+a);
+    if(value===a){
+            const firstStepcode = htmlFirstStepCodeExecute(value,obj[value]);
+            const responsibilityStepcode = htmlSecondStepCodeExecute(value,obj[value], Object.keys(obj[value].responsibilities).length);
+            const skillsEligibilityStepCode = htmlThirdStepCodeExecute(value,obj[value], Object.keys(obj[value].skillsEligibility).length);
+            const jobapplylink = htmlFourthStepCodeExecute(value,obj[value], Object.keys(obj[value].jobApplyURL).length)
+            console.log(value,obj[value])
+            document.querySelector('#main-content').insertAdjacentHTML('afterbegin',firstStepcode)
+            document.querySelector('#main-content').insertAdjacentHTML('beforeend',responsibilityStepcode) ;
+            document.querySelector('#main-content').insertAdjacentHTML('beforeend',skillsEligibilityStepCode);
+            document.querySelector('#main-content').insertAdjacentHTML('beforeend',jobapplylink);
+    }
+}
+
+// line no 176 add these lines when telegramand whatsapp addd
+// <div class="Telegram-whatsapp">
+// <img src="${o.imgSrcTelegram}" alt="telegram" srcset="">
+// <img src="${o.imgSrcWhatsapp}" alt="" srcset="">
+// </div> 
+function htmlFirstStepCodeExecute(value, o){
+    console.log('...0');    console.log(o);
+    const firstStep = `
+    <div class="content">
+            <p class="job-title">
+                '${o.jobTitle}'
+            </p>
+            <div class="details">
+                <span class="ti">Updated by : @JobUpdatesPortal,</span>
+                <span>an hours ago</span>
+            </div> 
+            
+            <p class="company-logo-img">
+            <img src="${o.imgSrc}" alt="" srcset="" >
+            </p>
+            
+            <p class="description">
+                ${o.description}
+            </p>
+            <h2>Job Description</h2>
+            <table>
+                <tr>
+                    <td class="heading">Company Name</td>
+                    <td >${o.companyName}</td>
+                </tr>
+                <tr>
+                    <td>Role</td>
+                    <td>${o.role}</td>
+                </tr>
+                <tr>
+                    <td>Location</td>
+                    <td>${o.location}</td>
+                </tr>
+                <tr>
+                    <td>Experience</td>
+                    <td>${o.experience}</td>
+                </tr>
+                <tr>
+                    <td>Qualification</td>
+                    <td>${o.qualification}</td>
+                </tr>
+            </table>`;
+              
+    return firstStep;
+}
+
+
+function htmlSecondStepCodeExecute(value,o,len){
+    console.log(value, o,  len);
+    let responsibilityStep ='';
+    for(let i=1; i<=len; i++){
+        let tag = '<li>'+ o.responsibilities[i] + '</li>';
+        responsibilityStep = responsibilityStep + tag;
+    }
+        console.log(responsibilityStep)
+        return   `<h2>Responsibilities</h2>
+                    <p class="Responsibilities">
+                    <ul>${responsibilityStep}</ul></p>`
+        
+}
+
+
+function htmlThirdStepCodeExecute(value,o,len){
+    console.log(value, o,  len);
+    let skillsEligibilityStep ='';
+    for(let i=1; i<=len; i++){
+        let tag = '<li>'+ o.skillsEligibility[i] + '</li>';
+        skillsEligibilityStep = skillsEligibilityStep + tag;
+    }
+        console.log(skillsEligibilityStep)
+        return   `<h2>Skills & Eligibility Criteria</h2>
+                    <p class="Responsibilities">
+                    <ul>${skillsEligibilityStep}</ul></p>`
+        
+}
+
+
+function htmlFourthStepCodeExecute(value,o,len){
+    console.log(value, o,  len);
+    let jobApplyURL ='';
+    for(let i=1; i<=len; i++){
+        let tag = `<li>Link : ${i} <a href=${o.jobApplyURL[i]}>Click Here</a></li>`;
+        jobApplyURL = jobApplyURL + tag;
+    }
+        console.log(jobApplyURL)
+        return   `<h2>Application Link</h2>
+                    <p class="Responsibilities">
+                    <ul>${jobApplyURL}</ul></p>`
+        
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const obj = {
+//     jobTitle : 'Apply Now | ServiceNow is hiring for Software Quality Engineer  !',
+//     imgSrc : 'https://d2djobalerts.github.io/images/serviceNow-logo.png',
+//     imgSrcTelegram : 'https://d2djobalerts.github.io/images/Telegram-join-image.png',
+//     imgSrcWhatsapp : 'https://d2djobalerts.github.io/images/Whatsapp-join-image.png',
+//     description : 'ServiceNow is looking for Software Quality Engineer with bachelors degree as qualification. The complete Googlemation like qualification, skills, etc are provided below.',
+//     companyName : 'ServiceNow',
+//     role : '',
+//     location : '',
+//     experience : '',
+//     qualification : '',
+    
+//     responsibilities : {
+//         1 : 'Design, develop, and execute test plans, test cases, and test scripts to thoroughly test software applications for functionality, performance, reliability, and scalability.',
+//         2  : 'Drive QA processes, standards, and methodologies to ensure consistent and high-quality deliverables.',
+//         3  : 'Collaborate closely with cross-functional teams, including software developers, product managers, and project managers, to identify and resolve quality issues in a timely manner.',
+//         4  : 'Monitor and analyze test results, identify trends, and provide recommendations for continuous improvement.',
+//         5  : 'Stay updated on industry trends, emerging technologies, and best practices in QA/testing methodologies to drive innovation and improvement.',
+//     },
+
+//     skillsEligibility : {
+//         1 : "Bachelor's degree in Computer Science, Engineering, or related field.",
+//         2 : 'Effective communication and interpersonal skills, with the ability to collaborate with cross-functional teams.',
+//         3 : 'Ability to work independently and manage multiple priorities in a fast-paced environment.',
+//         4 : 'Excellent analytical and problem-solving skills, with a keen attention to detail.',
+//         5 : 'Proven leadership abilities, with a track record of successfully leading and mentoring QA teams.',
+//         6 : 'Strong knowledge of software testing methodologies, tools, and techniques.',
+//     },
+
+//     howToApply : {
+//         1 : 'Read Job Description',
+//         2 : 'Click on below APPLY LINK',
+//         3 : 'Redirects to official page',
+//         4 : 'Fill formand APPLY'
+//     },
+
+//     jobApplyURL : 'https://careers.servicenow.com/en/jobs/744000000634596/software-quality-engineer/?trid=2d92f286-613b-4daf-9dfa-6340ffbecf73',
+//     telegramChannelURL : '',
+//     whatsAppChannelURL : '',
+//     imstagramChannelURL : '',
+// }
+
+
+
+
+
+// const html = `
+// <div class="content">
+//         <p class="job-title">
+//             ${obj.serviceNow.jobTitle}
+//         </p>
+//         <div class="details">
+//             <span class="ti">Updated by : @JobUpdatesPortal,</span>
+//             <span>an hours ago</span>
+//         </div> 
+        
+//         <img src="${obj.serviceNow.imgSrc}" alt="" srcset="">
+    
+//         <div class="Telegram-whatsapp">
+//             <img src="${obj.serviceNow.imgSrcTelegram}" alt="telegram" srcset="">
+//             <img src="${obj.serviceNow.imgSrcWhatsapp}" alt="" srcset="">
+//         </div> 
+//         <p class="description">
+//             ${obj.serviceNow.description}
+//         </p>
+//         <h2>Job Description</h2>
+//         <table>
+//             <tr>
+//                 <td class="heading">Company Name</td>
+//                 <td >${obj.serviceNow.companyName}</td>
+//             </tr>
+//             <tr>
+//                 <td>Role</td>
+//                 <td>${obj.serviceNow.role}</td>
+//             </tr>
+//             <tr>
+//                 <td>Location</td>
+//                 <td>${obj.serviceNow.location}</td>
+//             </tr>
+//             <tr>
+//                 <td>Experience</td>
+//                 <td>${obj.serviceNow.experience}</td>
+//             </tr>
+//             <tr>
+//                 <td>Qualification</td>
+//                 <td>${obj.serviceNow.qualification}</td>
+//             </tr>
+//         </table>
+
+        
+//         <h2>Responsibilities</h2>
+//         <p class="Responsibilities">
+//             <ul>
+//                 <li>${obj.serviceNow.responsibilities[1]}</li>
+//                 <li>${obj.serviceNow.responsibilities[2]}</li>
+//                 <li>${obj.serviceNow.responsibilities[3]}</li>
+//                 <li>${obj.serviceNow.responsibilities[4]}</li>
+//                 <li>${obj.serviceNow.responsibilities[5]}</li>
+//             </ul>
+//         </p>
+
+
+        
+//         <h2>Skills & Eligibility Criteria</h2>
+//         <p class="Responsibilities">
+//             <ul><li>${obj.serviceNow.skillsEligibility[1]}</li>
+//                 <li>${obj.serviceNow.skillsEligibility[2]}</li>
+//                 <li>${obj.serviceNow.skillsEligibility[3]}</li>
+//                 <li>${obj.serviceNow.skillsEligibility[4]}</li>
+//                 <li>${obj.serviceNow.skillsEligibility[5]}</li>
+//                 <li>${obj.serviceNow.skillsEligibility[6]}</li>
+//             </ul>
+//         </p>
+
+
+
+//         <h2>How to Apply?</h2>
+//         <p class="Responsibilities">
+//             <ul><li>${obj.serviceNow.howToApply[1]}</li>
+//                 <li>${obj.serviceNow.howToApply[2]}</li>
+//                 <li>${obj.serviceNow.howToApply[3]}</li>
+//                 <li>${obj.serviceNow.howToApply[4]}</li>
+//             </ul>
+//         </p>
+
+
+
+//         <h2 >Application Link : <a href="${obj.serviceNow.jobApplyURL}">Read More </a></h2>
+//         <h2 >Join Telegram community: <a href="${obj.serviceNow.telegramChannelURL}">Read More </a></h2>
+//         <h2 >Join WhatsApp community : <a href="${obj.serviceNow.whatsAppChannelURL}">Read More </a></h2>
+//         <h2 >Follow Instagram : <a href="${obj.serviceNow.imstagramChannelURL}">Read More </a></h2>
+        
+
+
+// </div>
+// `;
+
+// document.querySelector('#main-content').insertAdjacentHTML('afterbegin',html);
