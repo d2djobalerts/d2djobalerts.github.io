922d3d509930261b3cab7690437fd6a55f281451
d2djobalerts <d2djobalerts@gmail.com>
2024-07-29 12:54:27 +0530

    chages

---
+++ automateCodeJS/formScript.js
@@ -0,0 +1,8 @@
+const job_title = document.querySelector('.jobTitle');
+const image_src = document.querySelector('.imageSrc');
+const btn_submit = document.querySelector('.submit');
+
+btn_submit.addEventListener('click', function(){
+
+
+});
